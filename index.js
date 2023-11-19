a2ec446241139590f67d4df82bf9bb20e5d4a44e
dawalberto <albertoinformatica97@gmail.com>
2023-11-19 13:20:33 +0100

    endpoints getting body params

--- index.js
+++ index.js
@@ -19,26 +19,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.json());
 
-// Get
-app.get("/", async (req, res) => {});
-
 // Post - Generate extra keywords
 app.post("/generate-keywords", (req, res) => {
+  // Create bing chat instance
+  const api = new BingChat({ cookie: process.env.BING_COOKIE });
 
-	// Create bing chat instance
-	const api = new BingChat({ cookie: process.env.BING_COOKIE });
+  // Retrieve request information
+  const body = req.body;
+  const theme = body.theme;
+  if (!theme) {
+    res.json({ error: "no theme specified" });
+    return;
+  }
+  const keywords = "nature, forest, river, sunset"; // body.keywords;
+  const style = "realistic"; // body.style;
+  // const colors = "red, blue"; // body.colors;
+  //   console.log("body", body);
 
-	// Retrieve request information
-	const body = req.body;
-	const theme = "videogames and computers"; // body.theme;
-	const keywords = "gaming, mouse, keyboard, computer, esports"; // body.keywords;
-	const style = "realistic"; // body.style;
-	const colors = "red, blue"; // body.colors;
-	console.log("body", body);
-
-	// Generate extra keywords with bing chat
-	const prompt = 
-	`First, provide 10 words and short phrases related to ${theme}.
+  // Generate extra keywords with bing chat
+  const prompt = `First, provide 10 words and short phrases related to ${theme}.
 
 	Secondly, provide 10 extra words and short phrases related to ${keywords}.
 
@@ -47,57 +46,54 @@ app.post("/generate-keywords", (req, res) => {
 	IMPORTANT: Display only the generated words together in a sigle line format separated only by ",".
 	Do not display any other text within your response.`;
 
-	oraPromise(api.sendMessage(prompt),
-	{
-		text: prompt,
-		variant: "Precise"
-
-	}).then((result) => 
-	{
-		// Return generated keywords
-		console.log("Generated keywords: ", result.text);
-		res.json({ keywords : result.text });
-
-	}).catch((error) => 
-	{
-		res.json(error);
-	});
+  oraPromise(api.sendMessage(prompt), {
+    text: prompt,
+    variant: "Precise",
+  })
+    .then((result) => {
+      // Return generated keywords
+      console.log("Generated keywords: ", result.text);
+      res.json({ keywords: result.text });
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 // Post - Generate prompt
 app.post("/generate-prompt", (req, res) => {
+  // Create bing chat instance
+  const api = new BingChat({ cookie: process.env.BING_COOKIE });
 
-	// Create bing chat instance
-	const api = new BingChat({ cookie: process.env.BING_COOKIE });
+  // Retrieve keywords
+  const body = req.body;
+  if (!body.page || !body.keywords) {
+    res.json({ error: "no page or keywords specified" });
+    return;
+  }
+  const keywords = body.page + " webpage, " + body.keywords;
 
-	// Retrieve keywords
-	const keywords = "game, console, joystick, controller, graphics, resolution, FPS, CPU, GPU, RAM, gaming,mouse, keyboard, computer, esports, gaming chair, gaming headset, gaming monitor, gaming laptop, gaming desktop, realistic, 3D, texture, shading, lighting, rendering, modeling, animation, virtual reality, augmented reality";
-
-	// Generate prompt with bing chat
-	const prompt = 
-	`Instruction: Generate a detailed prompt for an image generator AI model taking into account the
+  // Generate prompt with bing chat
+  const prompt = `Instruction: Generate a detailed prompt for an image generator AI model taking into account the
 	following list of keywords: ${keywords}.
 
 	IMPORTANT: Display only the generated prompt. Do not display any other text within your response.`;
 
-	oraPromise(api.sendMessage(prompt),
-	{
-		text: prompt,
-		variant: "Precise"
-
-	}).then((result) => 
-	{
-		// Return generated prompt
-		console.log("Generated prompt: ", result.text);
-		res.json({ prompt : result.text });
-
-	}).catch((error) => 
-	{
-		res.json(error);
-	});
+  oraPromise(api.sendMessage(prompt), {
+    text: prompt,
+    variant: "Precise",
+  })
+    .then((result) => {
+      // Return generated prompt
+      console.log("Generated prompt: ", result.text);
+      res.json({ prompt: result.text });
+    })
+    .catch((error) => {
+      res.json(error);
+    });
 });
 
 // Start the server
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
